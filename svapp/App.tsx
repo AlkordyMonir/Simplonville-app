eb326cfff66d8f4c44603d61fd8be2efb812f10f
Monir ALKORDY <moneralkrd@gmail.com>
2024-04-02 03:05:07 +0200

    Add files via upload

---
+++ svapp/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
